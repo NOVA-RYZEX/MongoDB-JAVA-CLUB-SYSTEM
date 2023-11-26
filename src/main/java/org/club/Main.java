8178f4374ca8c5ef9678b905a0b62f8fddd2e0da
GIDEON KIPKEMOI YEBEI <145453820+Gideon-Yebei@users.noreply.github.com>
2023-11-26 10:52:14 +0300

    initial commit

--- src/main/java/org/club/Main.java
+++ src/main/java/org/club/Main.java
@@ -2,6 +2,10 @@ package org.club;
 
 import javax.swing.*;
 
+/**
+ * @author GKY
+ * @version 1.0 (26 Nov 2023)
+ */
 public class Main {
     public static void main(String[] args) {
         /*Load the required resources*/
