b2ed3723a8d6b9a0ec2a0a8b482e5e714f6dd291
GIDEON KIPKEMOI YEBEI <145453820+Gideon-Yebei@users.noreply.github.com>
2023-11-27 07:27:03 +0300

    GKY - InitialSetup

--- src/main/java/org/club/InitialSetup.java
+++ src/main/java/org/club/InitialSetup.java
@@ -3,5 +3,6 @@ package org.club;
 import javax.swing.*;
 
 public class InitialSetup extends JFrame {
+    /*TODO: make this class be first to run during the initial run to setup admin for the system */
 
 }
